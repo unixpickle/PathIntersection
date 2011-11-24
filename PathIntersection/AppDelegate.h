e6514a3ee2a4c7173df1d1d4d6daee2e44af5c7d
Alex Nichol <alexnichol@comcast.net>
2011-11-23 20:09:46 -0500

    Initial Commit

---
+++ PathIntersection/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PathIntersection
+//
+//  Created by Alex Nichol on 11/23/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
